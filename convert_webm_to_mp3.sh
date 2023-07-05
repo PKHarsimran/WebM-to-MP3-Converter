6a3ae92ba0b423db6e8ee8f334869e2826893af0
Harsimran Sidhu <22066581+PKHarsimran@users.noreply.github.com>
2023-07-05 15:00:48 +0000

    Update convert_webm_to_mp3.sh
    - Adding command-line arguments: This allows the user to specify the input and output directories instead of assuming the script will be run in the directory with the WebM files.
    - Adding error checks: These checks can handle common issues like missing dependencies or non-existing input directories.
    - Adding comments: While your script is fairly straightforward, comments can help others understand what's happening.

--- convert_webm_to_mp3.sh
+++ convert_webm_to_mp3.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-# This script will convert all WebM files in the current directory to MP3.
+# Check if ffmpeg is installed
+if ! [ -x "$(command -v ffmpeg)" ]; then
+  echo 'Error: ffmpeg is not installed.' >&2
+  exit 1
+fi
 
-for file in *.webm
+# Check if the input directory is specified and exists
+if [ -z "$1" ]; then
+    echo "Usage: $0 <input-directory> [output-directory]"
+    exit 1
+elif [ ! -d "$1" ]; then
+    echo "Error: $1 directory does not exist."
+    exit 1
+fi
+
+# Use the input directory as the output directory if none is specified
+OUTPUT_DIR=${2:-$1}
+
+# Convert all WebM files in the input directory to MP3 in the output directory
+for file in "$1"/*.webm
 do
-    ffmpeg -i "$file" -vn -codec:a libmp3lame -b:a 320k -ar 48000 "${file%.webm}.mp3"
+    # Extract the filename without the directory or extension
+    filename=$(basename -- "$file")
+    filename="${filename%.*}"
+    
+    # Convert the file to MP3
+    ffmpeg -i "$file" -vn -codec:a libmp3lame -b:a 320k -ar 48000 "$OUTPUT_DIR/$filename.mp3"
 done
